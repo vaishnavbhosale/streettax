b0f761f53fdbf8153357390f5f23de513adf1ee3
vaishnavbhosale <vaishnavbharatbhosale@gmail.com>
2026-01-24 17:58:12 +0530

    Testing

---
+++ src/main/java/com/streettax/streettax/StreettaxApplication.java
@@ -0,0 +1,13 @@
+package com.streettax.streettax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreettaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreettaxApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/streettax/streettax/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.streettax.streettax.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCHeck(){
+        return "vaishnavv";
+    }
+}
